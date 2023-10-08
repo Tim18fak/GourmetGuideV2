d5f9281ce3f32018b444f940d52a7d47dcbb03b1
Timothy Avell <timothy.avell.olatunde@gmail.com>
2023-10-08 19:12:15 -0400

    setup fot version2

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+module.exports = {
+  purge: [
+    './src/**/*.{js,jsx,ts,tsx}', // Your React source files
+  ],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
